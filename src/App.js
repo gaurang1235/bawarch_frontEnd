50d83728f066fbd5b6c05424fba86d6ce456a37d
Gaurang <gaurang1223@live.com>
2023-05-09 19:44:59 +0530

    SuperAdmin, FoodCourt complete, Restaurant View/Update Menu Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './screens/Login.js'
+import SuperAdmin from './screens/SuperAdmin';
+import FoodCourt from './screens/FoodCourt';
+import Restaurant from './screens/Restaurant';
+
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const loginHandler = (userObject) => {
+    console.log("hi i executed it");
+    console.log(userObject.role);
+    setUser(userObject);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user===null && (
+        <Login onLogin = {loginHandler}/>
+      )}
+      {user!==null && user.role==="ROLE_SUPER_ADMIN" && (
+        <SuperAdmin/>
+      )}
+      {user!==null && user.role==="ROLE_FOOD_COURT" && (
+        <FoodCourt user = {user}/>
+      )}
+      {user!==null && user.role==="ROLE_RESTAURANT" && (
+        <Restaurant rest = {user}/>
+      )}
     </div>
   );
 }
